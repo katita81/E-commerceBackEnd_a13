ae40ca9d1f1a4194457a08212a4e7b73c7588915
Katherine Echaiz <flor_echaiz@hotmail.com>
2023-02-19 11:37:35 +1100

    added more contents to README.md

--- Main/db/seeds.sql
+++ Main/db/seeds.sql
@@ -1,20 +1,20 @@
 INSERT INTO department (name)
 VALUES 
     ('Human Resources'),
-    ('Information Technology');
+    ('Information Technology'),
     ('Finance'),
     ('Marketing');
 
-INSERT INTO role (title, salary, department, role_id,manager_id)
+INSERT INTO role (title, salary, department)
 VALUES
-    ('Human Resources assistant',60000, 1, NULL),--role_id 
-    ('Human Resources Manager', 90000, 1, 1),--manager_id
-    ('Programmer', 100000, 2, NULL),--role_id 
-    ('Programming Manager', 200000, 2, 2),--manager_id
-    ('Accountant', 90000, 3, NULL),--role_id 
-    ('Finance Manager', 100000, 3, 3),--manager_id
-    ('Ilustrater', 80000, 4, NULL),--role_id 
-    ('Marketing Manager', 100000, 4, 4);--manager_id
+    ('Human Resources assistant',60000, 1), 
+    ('Human Resources Manager', 90000, 1),
+    ('Programmer', 100000, 2), 
+    ('Programming Manager', 200000, 2),
+    ('Accountant', 90000, 3), 
+    ('Finance Manager', 100000, 3),
+    ('Ilustrater', 80000, 4), 
+    ('Marketing Manager', 100000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
--- README.md
+++ README.md
@@ -21,6 +21,7 @@ THEN I am prompted to enter the name of the department and that department is ad
 
 WHEN I choose to add a role
 THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database.
+THEN I update schema.sql and seeds.sql by position me in the db folder and running mysql -u root -p, typing mysql password, source schema.sql, source seeds.sql, exit, back to main folder. run application and choose view role to see the updated role table with the added role.
 
 WHEN I choose to add an employee
 THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database.
